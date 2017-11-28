9b12678cb9590ef1ed4f41deef715b918fdc0711
lian.pengfei <lian.pengfei@ztesoft.com>
2017-11-28 13:35:20 +0800

    ss

--- Test1/AdvanceFeatur.py
+++ Test1/AdvanceFeatur.py
@@ -1,13 +1,18 @@
 # coding=gbk
 #切片
+print("切片")
+
 L = ['Michael', 'Sarah', 'Tracy', 'Bob', 'Jack']
 print(L[0:3])
 print(L[:3])
 print(L[-2:])
 print('ABCDEFG'[:3])
 
-# 迭代
+L= list(range(10))
+print(L)
 
+# 迭代
+print("迭代")
 d = {'a': 1, 'b': 2, 'c': 3}
 
 for key in d:
@@ -20,18 +25,25 @@ for key,valu in d.items():
 
 
 # 列表生成式
+print("列表生成式")
 print([x * x for x in range(1, 11)])
 print(range(1,11))
-print( [m + n for m in 'ABC' for n in 'XYZ'])
+print( [m + n  for m in 'ABC'  for n in 'XYZ'])
 
-print('=====')
 #生成器
+print('生成器')
+#所以，如果列表元素可以按照某种算法推算出来，那我们是否可以在循环的过程中不断推算出后续的元素呢？这样就不必创建完整的list，
+#从而节省大量的空间。在Python中，这种一边循环一边计算的机制，称为生成器：generator。
 g = (x * x for x in range(10))
+L = [x * x for x in range(10)]
 #python2.7
 #print(g.next())
 #python 3.6
 print(g.__next__())
-
+print("g is:")
+print(g)
+print("L is:")
+print(L)
 print('=====')
 for n in g:
     print(n)
@@ -44,9 +56,38 @@ def fib(max):
         n = n + 1
     return 'done'
 
+# a, b = b, a + b
+#t = (b, a + b) # t是一个tuple
+#a = t[0]
+#b = t[1]
+
 print('====fib(100)')
 fib(10)
 
+
+def fib2(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        yield b
+        a, b = b, a + b
+        n = n + 1
+    return 'done'
+
+print("获取 fib2 的 return值 ")
+g=fib2(4)
+while True:
+    try:
+        x = next(g)
+        print('g:', x)
+    except StopIteration as e:
+            print('Generator return value:', e.value)
+            break
+        
+        
+#generator和函数的执行流程不一样。
+#函数是顺序执行，遇到return语句或者最后一行函数语句就返回。
+#而变成generator的函数，
+#在每次调用next()的时候执行，遇到yield语句返回，再次执行时从上次返回的yield语句处继续执行。
 print('====odd(100)')
 #相当于一个生成器
 def odd():
@@ -58,7 +99,8 @@ def odd():
     yield(5)
 
 
-o=odd
+o=odd()
+print(o)
 print(next(o))
 print(next(o))
 print(next(o))
