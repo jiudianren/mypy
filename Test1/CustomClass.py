19494bae4a256cccf3505e7c847f4f26528c1410
jiudianren <648087012@qq.com>
2018-06-12 21:58:14 +0800

    s

---
+++ Test1/CustomClass.py
@@ -0,0 +1,14 @@
+# coding=gbk
+
+
+# python的类有一些 内置函数  就像c++中的重载操作符之类的
+# 可以自己实现，以覆盖系统默认的类的函数
+
+from ClassTest import  student
+
+LStu=student("li san",80)
+LStu.printStu()
+print(LStu)
+LStu
+
+print(student("mystu",100))
