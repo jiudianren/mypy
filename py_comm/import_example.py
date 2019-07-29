683e85ad479ec8eec5a4d92665b9c7a5799565f8
jiudianren <648087012@qq.com>
2019-07-29 15:35:07 +0800

    Create import_example.py

---
+++ py_comm/import_example.py
@@ -0,0 +1,17 @@
+# _*_ coding: utf-8 _*_
+# author jiudianren
+# time:2019/7/22 10:05
+
+
+# _*_ coding: utf-8 _*_
+# author jiudianren
+# time:2019/7/22 10:01
+'''
+将 这个文件 拷贝到 你的工程
+并在程序开始，引用这个函数
+'''
+
+import sys
+def import_comm():
+    path="F:\myworkgit\py_comm"
+    sys.path.append(path)
