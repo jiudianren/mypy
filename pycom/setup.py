3abdd28c7260c631fe6e6e47201da84dbad3581b
jiudianren <648087012@qq.com>
2019-11-05 18:42:58 +0800

    Create setup.py

---
+++ pycom/setup.py
@@ -0,0 +1,17 @@
+# _*_ coding: utf-8 _*_
+# author jiudianren
+# time:2019/11/5 16:02
+
+
+from setuptools import  setup
+
+
+setup(
+
+    name = "jiudianren_py_com",
+    version = '0.19.11.05',
+    long_description ="""
+    jiudianren's public tool for myself 
+    """,
+    install_requires =[]
+)
