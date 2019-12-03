1a6d6534307f3dda821670d83529f9df12f78db7
jiudianren <648087012@qq.com>
2019-12-03 09:46:24 +0800

    s

--- pycom/pycom/downloadGit.py
+++ pycom/pycom/downloadGit.py
@@ -11,25 +11,36 @@ import configparser
 def unzip_single(src_file, dest_dir):
     print("unzip_single start")
     f_str = src_file
-    zf = zipfile.ZipFile(f_str)
+
     try:
+        zf = zipfile.ZipFile(f_str)
         zf.extractall(path=dest_dir)
-    except RuntimeError as e:
-        print(e)
-    zf.close()
+        zf.close()
+    except Exception as e:
+        print(f"---***Exception:{e}  ***----")
+    finally:
+        print("finally do")
+
     print("unzip_single end")
 
 
 def download(url, zip_fn):
     r = requests.get(url.strip(), stream=True)
-    print(r.status_code)
+    print(f"status code:{r.status_code}" )
+
     if r.status_code != 200:
         print(r.headers)
         print("download Erro " + r.status_code)
         return False
 
     with open(zip_fn, "wb") as zipname:
-        for chunk in r.iter_content(chunk_size=1024): #边下载边存硬盘
+        data_size = 0
+        chunk_size = 16240
+        TE = r.headers.get('Transfer-Encoding')
+        print(f"Transfer-Encoding:{TE}")
+        for chunk in r.iter_content(chunk_size=chunk_size): #边下载边存硬盘
+            data_size += 1
+            print(f"data_size *{chunk_size}: {data_size}")
             if chunk:
                 zipname.write(chunk)
 
@@ -50,14 +61,14 @@ def deal_repository( repository_name):
     zip_filename = config[config_item]["zipfile"]
     uzip_dir = config[config_item]["uzipdir"]
 
-    print(zip_filename + uzip_dir)
+    print(zip_filename + "  "+uzip_dir)
 
     if download(url, zip_filename):
         unzip_single(zip_filename, uzip_dir)
 
 if __name__ == "__main__":
     deal_repository("mypy")
-
+    #deal_repository("jiudianren")
 
 
 
