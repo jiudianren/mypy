e44d20430035596c0a14dc7cd885d679cf9043ec
jiudianren <648087012@qq.com>
2020-01-19 10:18:46 +0800

    Create os_l.py

---
+++ pylearn/modle/innner_packet/os_l.py
@@ -0,0 +1,28 @@
+# _*_ coding: utf-8 _*_
+# author jiudianren
+# time:2020/1/17 14:58
+
+
+import os
+
+'''
+
+os.rename
+
+'''
+def test_os():
+    pass
+
+    path ="c:\\"
+    os.path.exists(path)
+    new_path="aa"
+    #os.mkdir()
+    os.getcwd()
+    #os.chdir()
+
+def os_cmd():
+    str = 'dir'
+    print(os.system(str))
+
+
+os_cmd()
