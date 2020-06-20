9ec087f60a59d72a7e04b92f338b3b2b3a569562
jiudianren <648087012@qq.com>
2020-06-20 10:43:59 +0800

    s

--- ThirdModel/readme.md
+++ ThirdModel/readme.md
@@ -1,11 +1,18 @@
 # 模块的大概功能
 
-#
+# 用过的模块
 
-#
+操作excel的pytho 模块 xld 
 
-#
+# html
+bs4
 
-#
+
+# 数据库
+pymysql
+
+# svn
+svn 
+
 #
 #
--- pycom/pycom/mylogger.py
+++ pycom/pycom/mylogger.py
@@ -9,6 +9,20 @@ import logging
 
 def pylearnlog( log_level=logging.DEBUG, name="default_name"):
     # log实例
+    '''
+     format: 指定输出的格式和内容，format可以输出很多有用信息，如上例所示:
+     %(levelno)s: 打印日志级别的数值
+     %(levelname)s: 打印日志级别名称
+     %(pathname)s: 打印当前执行程序的路径，其实就是sys.argv[0]
+     %(filename)s: 打印当前执行程序名
+     %(funcName)s: 打印日志的当前函数
+     %(lineno)d: 打印日志的当前行号
+     %(asctime)s: 打印日志的时间
+     %(thread)d: 打印线程ID
+     %(threadName)s: 打印线程名称
+     %(process)d: 打印进程ID
+     %(message)s: 打印日志信息
+ '''
     logging.basicConfig(format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s')
     re = logging.getLogger(name)
     # 创建handler 写到控制台，或者日志文件等
